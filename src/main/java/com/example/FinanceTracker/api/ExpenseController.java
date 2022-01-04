ee2450497a90db2de92b5c59208fae8d302f06a8
echoaj <aljoslin13@yahoo.com>
2022-01-04 00:59:57 -0800

    added comment

--- src/main/java/com/example/FinanceTracker/api/ExpenseController.java
+++ src/main/java/com/example/FinanceTracker/api/ExpenseController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins="https://finance-tracker-js.herokuapp.com")
+//@CrossOrigin(origins="http://localhost:3000")
 @RequestMapping("api/expense")
 @RestController
 public class ExpenseController {
